5078602d08a616dfa954bd5700e222f4f584b0a3
ardit <kosinoff.jenia1226@gmail.com>
2023-05-08 10:55:37 +0300

    Updating password

--- send_email.py
+++ send_email.py
@@ -6,9 +6,9 @@ import os
 def send_email(message):
     host = 'smtp.gmail.com'
     port = 465
-    user_name = 'atovvetron@gmail.com'
+    user_name = 'blackardit3@gmail.com'
     password = os.getenv('PASSWORD')
-    receiver = 'atovvetron@gmail.com'
+    receiver = 'blackardit3@gmail.com'
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
